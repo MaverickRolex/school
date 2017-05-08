877b9276ef16db7a75f305d7960e332ee98cb947
Alexis Rabago <alexisraca@hotmail.com>
2017-05-08 13:03:00 -0500

    fix database_name add root

--- db/migrate/20170508170720_devise_create_users.rb
+++ db/migrate/20170508170720_devise_create_users.rb
@@ -32,6 +32,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
 
 
       t.timestamps null: false
+
+      # Adding fields from user table
+      t.string :user_name
+      t.string :addres
+      t.string :phone
+      t.boolean :department_boss
+      t.integer :department_id
+
     end
 
     add_index :users, :email,                unique: true
--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,11 @@ ActiveRecord::Schema.define(version: 20170508170727) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "user_name"
+    t.string   "addres"
+    t.string   "phone"
+    t.boolean  "department_boss"
+    t.integer  "department_id"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
